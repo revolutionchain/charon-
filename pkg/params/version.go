9b6ad418f0bab0e4b0730820571114bbf4889a1a
Chase Ripplinger <chase@ripply.io>
2022-08-08 08:19:25 -0700

    Bump version

--- pkg/params/version.go
+++ pkg/params/version.go
@@ -6,7 +6,7 @@ const (
 	unstable     = "unstable"
 	stable       = "stable"
 	VersionMajor = 0        // Major version component of the current release
-	VersionMinor = 2        // Minor version component of the current release
+	VersionMinor = 3        // Minor version component of the current release
 	VersionPatch = 0        // Patch version component of the current release
 	VersionMeta  = unstable // Version metadata to append to the version string
 )
